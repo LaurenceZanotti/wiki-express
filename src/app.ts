35ddba457a752ece003084fcfd080a5c6e035aa8
Laurence Zanotti <lau_zanotti@hotmail.com>
2023-04-15 18:10:30 -0300

    feat: setup and play with ejs

--- src/app.ts
+++ src/app.ts
@@ -5,4 +5,8 @@ dotenv.config()
 const port = process.env.API_PORT || 3000
 const app = express()
 
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index', { title: "Salve", message:"Aoba aoba, d boa?"}))
+
 app.listen(port, () => console.log(`App running at http://localhost:${port}`))
