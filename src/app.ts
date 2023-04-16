ffd34019e72bfe62a1ed6ed05b843b6b5b49cedb
Laurence Zanotti <lau_zanotti@hotmail.com>
2023-04-16 00:06:32 -0300

    feat: use async capabilities

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { list_entries, save_entry, get_entry } from './util'
 
-dotenv.config()
-const port = process.env.API_PORT || 3000
-const app = express()
 
-app.set('view engine', 'ejs')
+async function main() {
+    dotenv.config()
+    const port = process.env.API_PORT || 3000
+    const app = express()
+    
+    app.set('view engine', 'ejs')
+    
+    app.get('/', (req, res) => res.render('index', { title: "Salve", message:"Aoba aoba, d boa?"}))
+    
+    app.listen(port, () => console.log(`App running at http://localhost:${port}`))
+}
 
-app.get('/', (req, res) => res.render('index', { title: "Salve", message:"Aoba aoba, d boa?"}))
 
-app.listen(port, () => console.log(`App running at http://localhost:${port}`))
+main()
