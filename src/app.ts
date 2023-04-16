c341f13da6d565bc80bdef550f3ac5c1c9b31d42
Laurence Zanotti <lau_zanotti@hotmail.com>
2023-04-16 01:30:55 -0300

    chore: change env var name

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { list_entries, save_entry, get_entry } from './util'
 
 async function main() {
     dotenv.config()
-    const port = process.env.API_PORT || 3000
+    const port = process.env.SERVER_PORT || 3000
     const app = express()
     
     app.set('view engine', 'ejs')
